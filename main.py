65b09d6316ac68b45d564c7fa7c9f29ba3ec67c6
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-30 11:14:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Katelyn Curtiss
+# October 30 2024
+# List Elemnts
+
+
+# Part 1
+favorite_snacks = ['chocolate' , 'onion rings' , 'candy ', 'ice cream' , 'doritos']
+print(favorite_snacks)
+
+
+
+# Pop() method
+favorite_snacks.pop('doritos')
+print(favorite_snacks)
+favorite_snacks.pop(2)
+
+# Remove() method
+favorite_snacks.remove('candy')
+print(favorite_snacks)
+
+# Keyword DEL
+
+
